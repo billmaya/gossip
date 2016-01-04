77f97c2e6de68903b71c8d3ca5e36774849d331b
Bill Maya <billmaya@NX-01.local>
2016-01-04 10:41:19 -0500

    Used correct screen coordinates to draw black background and help images.

--- Gossip/Views/ViewController/HelpView.cs
+++ Gossip/Views/ViewController/HelpView.cs
@@ -39,42 +39,42 @@ namespace Gossip
 			using (CGContext gctx = UIGraphics.GetCurrentContext ()) {
 
 				gctx.SetFillColor(UIColor.Black.CGColor);
-				gctx.FillRect(new RectangleF(0, 0, UIScreen.MainScreen.Bounds.Height, UIScreen.MainScreen.Bounds.Width));
+				gctx.FillRect(new RectangleF(0, 0, UIScreen.MainScreen.Bounds.Width, UIScreen.MainScreen.Bounds.Height));
 			
 
 				switch (index) 
 				{
 				case 0:
 					if (ipad) helpImage = Resources.rulesGame;
-					else helpImage = Resources.rulesGame.Scale(new SizeF(UIScreen.MainScreen.Bounds.Height * scaleBy, UIScreen.MainScreen.Bounds.Width * scaleBy));
+					else helpImage = Resources.rulesGame.Scale(new SizeF(UIScreen.MainScreen.Bounds.Width * scaleBy, UIScreen.MainScreen.Bounds.Height * scaleBy));
 					break;
 				case 1:
 					if (ipad) helpImage = Resources.rulesBara;
-					else helpImage = Resources.rulesBara.Scale(new SizeF(UIScreen.MainScreen.Bounds.Height * scaleBy, UIScreen.MainScreen.Bounds.Width * scaleBy));
+					else helpImage = Resources.rulesBara.Scale(new SizeF(UIScreen.MainScreen.Bounds.Width * scaleBy, UIScreen.MainScreen.Bounds.Height * scaleBy));
 					break;
 				case 2:
 					if (ipad) helpImage = Resources.rulesOwen;
-					else helpImage = Resources.rulesOwen.Scale(new SizeF(UIScreen.MainScreen.Bounds.Height * scaleBy, UIScreen.MainScreen.Bounds.Width * scaleBy));
+					else helpImage = Resources.rulesOwen.Scale(new SizeF(UIScreen.MainScreen.Bounds.Width * scaleBy, UIScreen.MainScreen.Bounds.Height * scaleBy));
 					break;
 				case 3:
 					if (ipad) helpImage = Resources.rulesMax;
-					else helpImage = Resources.rulesMax.Scale(new SizeF(UIScreen.MainScreen.Bounds.Height * scaleBy, UIScreen.MainScreen.Bounds.Width * scaleBy));
+					else helpImage = Resources.rulesMax.Scale(new SizeF(UIScreen.MainScreen.Bounds.Width * scaleBy, UIScreen.MainScreen.Bounds.Height * scaleBy));
 					break;
 				case 4:
 					if (ipad) helpImage = Resources.rulesElla;
-					else helpImage = Resources.rulesElla.Scale(new SizeF(UIScreen.MainScreen.Bounds.Height * scaleBy, UIScreen.MainScreen.Bounds.Width * scaleBy));
+					else helpImage = Resources.rulesElla.Scale(new SizeF(UIScreen.MainScreen.Bounds.Width * scaleBy, UIScreen.MainScreen.Bounds.Height * scaleBy));
 					break;
 				case 5:
 					if (ipad) helpImage = Resources.rulesMort;
-					else helpImage = Resources.rulesMort.Scale(new SizeF(UIScreen.MainScreen.Bounds.Height * scaleBy, UIScreen.MainScreen.Bounds.Width * scaleBy));
+					else helpImage = Resources.rulesMort.Scale(new SizeF(UIScreen.MainScreen.Bounds.Width * scaleBy, UIScreen.MainScreen.Bounds.Height * scaleBy));
 					break;
 				case 6:
 					if (ipad) helpImage = Resources.rulesZoe;
-					else helpImage = Resources.rulesZoe.Scale(new SizeF(UIScreen.MainScreen.Bounds.Height * scaleBy, UIScreen.MainScreen.Bounds.Width * scaleBy));
+					else helpImage = Resources.rulesZoe.Scale(new SizeF(UIScreen.MainScreen.Bounds.Width * scaleBy, UIScreen.MainScreen.Bounds.Height * scaleBy));
 					break;
 				}
 
-				helpImage.Draw(new PointF((UIScreen.MainScreen.Bounds.Height - helpImage.Size.Width) / 2f, 0f));
+				helpImage.Draw(new PointF((UIScreen.MainScreen.Bounds.Width - helpImage.Size.Width) / 2f, 0f));
 
 				this.AddSubview(leftArrowHelpView);
 				leftArrowHelpView.SetNeedsDisplay();
